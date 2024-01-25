61d2f70fe47f462593d9cdb4eb774242b5e8c27d
o2real <136673119+o2real@users.noreply.github.com>
2024-01-25 00:40:30 +0000

    return

--- nomard.py
+++ nomard.py
@@ -36,7 +36,7 @@ def pay_tax(tax):
 to_pay = tax_calculator(1500000)
 pay_tax(to_pay)
 
-# tax_calculator(input("How much money did you make this year? $ "))
+tax_calculator(input("How much money did you make this year? $ "))
 
 # 1/24
 def say_hello(name = "anonymous"):
@@ -47,3 +47,32 @@ say_hello()
 
 def plus(a,b):
   print(a + b)
+
+
+def pay_tax(tax):
+  print("thank you for paying", tax)
+
+to_pay = tax_calculator(1500000)
+pay_tax(to_pay)
+
+my_name = "Jioh"
+my_age = 28
+my_color_eyes = "brown"
+
+print(f"Hello i'm {my_name}, I have {my_age} years old, {my_color_eyes} is my eye color.")
+
+def make_juice(fruit):
+  return f"{fruit} + J"
+
+def add_ice(juice):
+  return f"{juice} + I"
+
+def add_sugar(ice_juice):
+  return f"{ice_juice} + S"
+
+juice = make_juice("사과")
+cold_juice = add_ice(juice)
+perfect_juice = add_sugar(cold_juice)
+
+print(perfect_juice)
+
