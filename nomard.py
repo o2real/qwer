75a75b8763e5db6448fc44ccabaa28792bfd444e
o2real <136673119+o2real@users.noreply.github.com>
2024-01-31 01:07:03 +0000

    again

--- nomard.py
+++ nomard.py
@@ -295,85 +295,39 @@
 
 # bibi.sleep()
 
-# class player:
+class player:
 
-#   def __init__(self, name, team):
-#     self.name = name
-#     self.xp = 1500
-#     self.team = team
+  def __init__(self, name, team):
+    self.name = name
+    self.xp = 1500
+    self.team = team
 
-#   def introduce(self):
-#     print(f"Hello I am {self.name} and I play for {self.team}.")
+  def introduce(self):
+    print(f"Hello I am {self.name} and I play for {self.team}.")
 
-# class team:
+class team:
 
-#   def __init__(self, team_name):
-#     self.teamname = team_name
-#     self.players = []
+  def __init__(self, team_name):
+    self.teamname = team_name
+    self.players = []
 
-#   def show_players(self):
-#     for player in self.players:
-#       player.introduce()
+  def show_players(self):
+    for player in self.players:
+      player.introduce()
   
-#   def add_player(self, name):
-#     new_player = player(name, self.teamname)
-#     self.players.append(new_player)
+  def add_player(self, name):
+    new_player = player(name, self.teamname)
+    self.players.append(new_player)
 
 
-# team_X = team("team X")
+team_X = team("team X")
 
-# team_X.add_player("Jioh")
+team_X.add_player("Jioh")
 
-# team_blue = team("team blue")
+team_blue = team("team blue")
 
-# team_blue.add_player("Yeon")
+team_blue.add_player("Yeon")
 
-# team_blue.show_players()
-
-
-import requests
-from bs4 import BeautifulSoup
+team_blue.show_players()
 
 
-all_jobs = []
-
-
-
-
-def scrape_page(url):
-  print(f"Scrapping{url}...")
-  response = requests.get(url)
-  
-  soup = BeautifulSoup(response.content, "html.parser",)
-  
-  jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
-  
-  for job in jobs:
-    title = job.find("span", class_="title").text
-    company, position, region = job.find_all("span", class_="company")
-  
-    url = job.find("div", class_="tooltip").next_sibling["href"]
-  
-    job_data = {
-      "title" : title,
-      "company" : company.text,
-      "position" : position.text,
-      "region" : region.text,
-      "url" : f"https://weworkremotely.com{url}",
-    }
-    all_jobs.append(job_data)
-
-def get_pages(url):
-  response = requests.get(url)
-  
-  soup = BeautifulSoup(response.content, "html.parser")
-  
-  return len(soup.find("div", class_="pagination").find_all("span",class_="page"))
-
-total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
-
-for x in range(total_pages):
-  url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
-  scrape_page(url)
-
-print(len(all_jobs))
