b9ba182843d290958d6bcf018186bee982541135
o2real <136673119+o2real@users.noreply.github.com>
2024-02-03 14:46:34 +0000

    whay

--- nomard.py
+++ nomard.py
@@ -339,53 +339,64 @@
 
 
 
-def scrape_page(url):
-  print(f"Scrapping{url}...")
-  response = requests.get(url)
+# def scrape_page(url):
+#   print(f"Scrapping{url}...")
+#   response = requests.get(url)
 
-  soup = BeautifulSoup(response.content, "html.parser",)
+#   soup = BeautifulSoup(response.content, "html.parser",)
 
-  jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+#   jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
-  for job in jobs:
-    title = job.find("span", class_="title").text
-    company, position, region = job.find_all("span", class_="company")
+#   for job in jobs:
+#     title = job.find("span", class_="title").text
+#     company, position, region = job.find_all("span", class_="company")
 
-    url = job.find("div", class_="tooltip").next_sibling["href"]
+#     url = job.find("div", class_="tooltip").next_sibling["href"]
 
-    job_data = {
-      "title" : title,
-      "company" : company.text,
-      "position" : position.text,
-      "region" : region.text,
-      "url" : f"https://weworkremotely.com{url}",
-    }
-    all_jobs.append(job_data)
+#     job_data = {
+#       "title" : title,
+#       "company" : company.text,
+#       "position" : position.text,
+#       "region" : region.text,
+#       "url" : f"https://weworkremotely.com{url}",
+#     }
+#     all_jobs.append(job_data)
 
-def get_pages(url):
-  response = requests.get(url)
+# def get_pages(url):
+#   response = requests.get(url)
 
-  soup = BeautifulSoup(response.content, "html.parser")
+#   soup = BeautifulSoup(response.content, "html.parser")
 
-  return len(soup.find("div", class_="pagination").find_all("span",class_="page"))
+#   return len(soup.find("div", class_="pagination").find_all("span",class_="page"))
 
-total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+# total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
 
-for x in range(total_pages):
-  url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
-  scrape_page(url)
+# for x in range(total_pages):
+#   url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
+#   scrape_page(url)
 
-print(len(all_jobs))
+# print(len(all_jobs))
 
 
-keywords = ["flutter", "python", "golang"]
+# keywords = ["flutter", "python", "golang"]
 
-r = requests.get(
-    "https://remoteok.com/remote-flutter-jobs",
-    headers={
-        "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Whale/3.24.223.21 Safari/537.36"
-    })
+# r = requests.get(
+#     "https://remoteok.com/remote-flutter-jobs",
+#     headers={
+#         "User-Agent":
+#         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Whale/3.24.223.21 Safari/537.36"
+#     })
 
-print(r.status_code)
+# print(r.status_code)
 
+
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
+keyword = input("What do you want to search for?")
+
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
+jobs = indeed + wwr
+
+save_to_file(keyword, jobs)
