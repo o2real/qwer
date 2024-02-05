71071af5eeef01e68a7eb21d5fc855bb2fce8016
o2real <136673119+o2real@users.noreply.github.com>
2024-02-05 14:29:32 +0000

    zx,cmv

--- flie.py
+++ flie.py
@@ -3,8 +3,6 @@ def save_to_file(file_name,job):
   file.write("Position,Company,Location,URL\n")
 
   for job in jobs:
-      file.write(
-          f"{job['position']},{job['company']},{job['location']},{job['link']}\n"
-    )
+    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
 
-  file. close()
+    file. close()
